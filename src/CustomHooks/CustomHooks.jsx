832e1314e2c5baf7a4273e3a37b1d6bca2eb29c9
Avinash Prajapati <avinashprajapati914@gmail.com>
2023-10-31 00:36:41 +0530

    fix: eslint

--- src/CustomHooks/CustomHooks.jsx
+++ src/CustomHooks/CustomHooks.jsx
@@ -12,7 +12,6 @@ function useCloseOnClickOutside(ref, handler = '') {
     return () => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [ref]);
 }
 
